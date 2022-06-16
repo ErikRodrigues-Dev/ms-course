292806ab92ad54ee85cdee6fa13b3f0383ce40e6
ErikRodrigues-Dev <erik.rodrigues.dev@gmail.com>
2022-06-16 09:36:56 -0300

    Projeto Hr-worker Criado

---
+++ hr-worker/src/test/java/com/cursodev/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursodev.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
